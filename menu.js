17f0c52ec995a82f134ba3f1eb0d9721a970e825
Harshad-Gore <202301040138@mitaoe.ac.in>
2024-10-07 00:35:34 +0530

    major update

--- menu.js
+++ menu.js
@@ -5,3 +5,13 @@ function toggleMenu() {
     menuIcon.classList.toggle('active');
     dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
 }
+
+// Function to close the dropdown menu if clicked outside
+window.onclick = function(event) {
+    const dropdownMenu = document.getElementById('dropdown-menu');
+    const menuIcon = document.querySelector('.menu-icon');
+    if (!menuIcon.contains(event.target) && !dropdownMenu.contains(event.target)) {
+        dropdownMenu.style.display = 'none';
+        menuIcon.classList.remove('active');
+    }
+}
